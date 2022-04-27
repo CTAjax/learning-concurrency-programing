10c3c92701b89eac478fee341026e8e27fde9fac
ajax <ajaxliu@chancetop.com>
2022-04-27 10:03:42 +0800

    add multiple testing

--- chapter-2/src/main/java/YahooFinance.java
+++ chapter-2/src/main/java/YahooFinance.java
@@ -1,20 +1,28 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 /**
  * @author ajax
  */
 public class YahooFinance {
-    public static void main(String[] args) throws IOException {
-        getPrice();
-    }
-
-    public static void getPrice() throws IOException {
-        final URL url = new URL("https://www.baidu.com");
-        final BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
-        final String data = reader.readLine();
-        System.out.println(data);
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+        final List<Callable<Double>> partitions = new ArrayList<>();
+        List<Future<Double>> feature = executor.invokeAll(partitions);
+        double sum = feature.stream().mapToDouble(doubleFuture -> {
+            try {
+                return doubleFuture.get();
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+            return 0.0d;
+        }).sum();
+        System.out.println(sum);
     }
 }
